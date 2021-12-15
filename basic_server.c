7d14ddf2f2f1096eb80cb1252823e7b32503330e
KenLi8504 <kli22@stuy.edu>
2021-12-15 04:39:47 -0500

    Infinite loop attempt #2

--- basic_server.c
+++ basic_server.c
@@ -13,53 +13,55 @@ int main() {
       from_client = server_handshake( &to_client );
       hasClient = hasClient + 1;
     }
-    char input[10000];
-    char intermediate[10000];
-    char output [10000];
+    while(1){
+      char input[10000];
+      char intermediate[10000];
+      char output [10000];
 
-    // char *toChild = "toChild";
-    // char *toParent = "toParent";
-    // int pipeto = open(toChild,O_RDONLY);
-    // int pipefrom = open(toParent,O_WRONLY);
-    //printf("The values of my pipes are %d and %d\n",pipeto,pipefrom);
-    while (1){
-      read(from_client,input,10000);
-      printf("Party!\n");
-      strcpy(intermediate,input);
-      printf("The child here received here was %s\n",input);
-      //printf("The output here was %s\n",intermediate);
-      int counter = 0;
-      for (int i = 0; intermediate[i]!='\0'; i++) {
-        //printf("This char is %c\n",intermediate[i]);
-        //printf("The counter is %d",counter);
-        if (counter == 0){
-          if(intermediate[i] >= 'a' && intermediate[i] <= 'z') {
-            intermediate[i] = intermediate[i] - 32;
-            counter = counter + 1;
-            //printf("OPTION 1");
+      // char *toChild = "toChild";
+      // char *toParent = "toParent";
+      // int pipeto = open(toChild,O_RDONLY);
+      // int pipefrom = open(toParent,O_WRONLY);
+      //printf("The values of my pipes are %d and %d\n",pipeto,pipefrom);
+      while (1){
+        read(from_client,input,10000);
+        printf("Party!\n");
+        strcpy(intermediate,input);
+        printf("The child here received here was %s\n",input);
+        //printf("The output here was %s\n",intermediate);
+        int counter = 0;
+        for (int i = 0; intermediate[i]!='\0'; i++) {
+          //printf("This char is %c\n",intermediate[i]);
+          //printf("The counter is %d",counter);
+          if (counter == 0){
+            if(intermediate[i] >= 'a' && intermediate[i] <= 'z') {
+              intermediate[i] = intermediate[i] - 32;
+              counter = counter + 1;
+              //printf("OPTION 1");
+            }
+            else if (intermediate[i] >= 'A' && intermediate[i] <= 'Z'){
+              counter = counter + 1;
+              //printf("OPTION 2");
+            }
           }
-          else if (intermediate[i] >= 'A' && intermediate[i] <= 'Z'){
-            counter = counter + 1;
-            //printf("OPTION 2");
+          else if (counter == 1){
+            if (intermediate[i] >= 'a' && intermediate[i] <= 'z') {
+              counter = counter - 1;
+              //printf("OPTION 3");
+            }
+            else if (intermediate[i] >= 'A' && intermediate[i] <= 'Z'){
+              intermediate[i] = intermediate[i] + 32;
+              counter = counter - 1;
+              //printf("OPTION 4");
+            }
           }
+        printf("The new string is %s",intermediate);
         }
-        else if (counter == 1){
-          if (intermediate[i] >= 'a' && intermediate[i] <= 'z') {
-            counter = counter - 1;
-            //printf("OPTION 3");
-          }
-          else if (intermediate[i] >= 'A' && intermediate[i] <= 'Z'){
-            intermediate[i] = intermediate[i] + 32;
-            counter = counter - 1;
-            //printf("OPTION 4");
-          }
-        }
-      printf("The new string is %s",intermediate);
+        strcpy(output,intermediate);
+        // printf("The length of the input is %ld\n",strlen(input));
+        printf("The output here was %s\n",intermediate);
+        write(to_client,output,10000);
       }
-      strcpy(output,intermediate);
-      // printf("The length of the input is %ld\n",strlen(input));
-      printf("The output here was %s\n",intermediate);
-      write(to_client,output,10000);
     }
   }
   return 0;
