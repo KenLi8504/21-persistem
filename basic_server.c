d05dc24e6ea1d2cf64c9e2ad8c2f3c277bd1d54d
KenLi8504 <kli22@stuy.edu>
2021-12-15 05:03:40 -0500

    more debugging

--- basic_server.c
+++ basic_server.c
@@ -16,48 +16,48 @@ int main() {
         char intermediate[10000];
         char output [10000];
 
-        if (read(from_client,input,1000) != 0){
-          printf("the input was %s\n",input);
-          printf("Party!\n");
-          strcpy(intermediate,input);
-          printf("The child here received here was %s\n",input);
-          //printf("The output here was %s\n",intermediate);
-          int counter = 0;
-          for (int i = 0; intermediate[i]!='\0'; i++) {
-            //printf("This char is %c\n",intermediate[i]);
-            //printf("The counter is %d",counter);
-            if (counter == 0){
-              if(intermediate[i] >= 'a' && intermediate[i] <= 'z') {
-                intermediate[i] = intermediate[i] - 32;
-                counter = counter + 1;
-                //printf("OPTION 1");
-              }
-              else if (intermediate[i] >= 'A' && intermediate[i] <= 'Z'){
-                counter = counter + 1;
-                //printf("OPTION 2");
-              }
-            }
-            else if (counter == 1){
-              if (intermediate[i] >= 'a' && intermediate[i] <= 'z') {
-                counter = counter - 1;
-                //printf("OPTION 3");
-              }
-              else if (intermediate[i] >= 'A' && intermediate[i] <= 'Z'){
-                intermediate[i] = intermediate[i] + 32;
-                counter = counter - 1;
-                //printf("OPTION 4");
-              }
-            }
-          printf("The new string is %s",intermediate);
-          }
-          strcpy(output,intermediate);
-          // printf("The length of the input is %ld\n",strlen(input));
-          printf("The output here was %s\n",intermediate);
-          write(to_client,output,10000);
-        }
-        else{
+        int test = read(from_client,input,10000);
+
+        if (test == 0){
           break;
         }
+
+        printf("Party!\n");
+        strcpy(intermediate,input);
+        printf("The child here received here was %s\n",input);
+        //printf("The output here was %s\n",intermediate);
+        int counter = 0;
+        for (int i = 0; intermediate[i]!='\0'; i++) {
+          //printf("This char is %c\n",intermediate[i]);
+          //printf("The counter is %d",counter);
+          if (counter == 0){
+            if(intermediate[i] >= 'a' && intermediate[i] <= 'z') {
+              intermediate[i] = intermediate[i] - 32;
+              counter = counter + 1;
+              //printf("OPTION 1");
+            }
+            else if (intermediate[i] >= 'A' && intermediate[i] <= 'Z'){
+              counter = counter + 1;
+              //printf("OPTION 2");
+            }
+          }
+          else if (counter == 1){
+            if (intermediate[i] >= 'a' && intermediate[i] <= 'z') {
+              counter = counter - 1;
+              //printf("OPTION 3");
+            }
+            else if (intermediate[i] >= 'A' && intermediate[i] <= 'Z'){
+              intermediate[i] = intermediate[i] + 32;
+              counter = counter - 1;
+              //printf("OPTION 4");
+            }
+          }
+        printf("The new string is %s",intermediate);
+        }
+        strcpy(output,intermediate);
+        // printf("The length of the input is %ld\n",strlen(input));
+        printf("The output here was %s\n",intermediate);
+        write(to_client,output,10000);
     }
   }
   return 0;
